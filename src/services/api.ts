3ff21ab2760c9092f1fcdebd17fb809523d1b876
Moisés <moisesnetored@gmail.com>
2023-05-08 13:40:12 -0400

    feat: refreshing the token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+/* eslint-disable prefer-const */
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
+
+interface AxiosErrorResponse {
+  code?: string
+}
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -9,3 +14,47 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies['@nextauth.token']}`,
   },
 })
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error: AxiosError<AxiosErrorResponse>) => {
+    console.log('ops', error.response?.status)
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        // renovar o token
+        cookies = parseCookies()
+
+        const { '@nextauth.refreshToken': refreshToken } = cookies
+
+        api
+          .post('/refresh', {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response.data
+
+            setCookie(undefined, '@nextauth.token', token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: '/',
+            })
+
+            setCookie(
+              undefined,
+              '@nextauth.refreshToken',
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/',
+              },
+            )
+
+            api.defaults.headers.Authorization = `Bearer ${token}`
+          })
+      } else {
+        // deslogar usuário
+      }
+    }
+  },
+)
